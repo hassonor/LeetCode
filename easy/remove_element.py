1a05c1ffd5a04a4080b479bb183e45f72728196d
Or Hasson <hassonor@gmail.com>
2024-11-11 17:03:21 +0200

    remove element solve

---
+++ easy/remove_element.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def remove_element(self, nums: List[int], val: int) -> int:
+    if not nums:
+        return 0
+
+    k = 0
+
+    for num in nums:
+        if num != val:
+            nums[k] = num
+            k += 1
+
+    return k
